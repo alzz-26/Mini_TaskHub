848f8b61807a120f3f54b06133b1bde55a5f7b29
Alisha Basa <56277823+alzz-26@users.noreply.github.com>
2025-04-23 00:01:47 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ import 'auth/splash_screen.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://sqicxmznmunsrjntgama.supabase.co', // Supabase URL
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxaWN4bXpubXVuc3JqbnRnYW1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyNjU2NzIsImV4cCI6MjA2MDg0MTY3Mn0._38wGvC0_VhAnN4Ktc2R8ilUkQjuHMVfyx1c4uZEbYQ', // Replace with your Supabase anon key
+    url: '', // Replace with your Supabase project URL
+    anonKey: '', // Replace with your Supabase anon key
   );
   runApp(const MyApp());
 }
